7b337b9b68d8ebf56802d195d8fbd9d7be5ada3f
Navin <navin.lubhaya@proagrica.com>
2019-04-17 17:44:41 +0100

    working db

--- LMS/src/main/java/com/lms/controllers/rest/MainRestController.java
+++ LMS/src/main/java/com/lms/controllers/rest/MainRestController.java
@@ -1,14 +1,29 @@
 package com.lms.controllers.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lms.services.LmsService;
+import com.lms.models.Book;
+
+import java.util.Collection;
+
 
 @RestController
 public class MainRestController {
 	
+	@Autowired
+	LmsService lmsService;
+	
 	@GetMapping(value="/")
 	public String hello() {
 		return "Hello World!";
+	}
+	
+	@GetMapping(value="/findAllBooks")
+	public Collection<Book> getAllBooks() {
+		return lmsService.findAllBooks();
 	}
 
 }
--- LMS/src/main/java/com/lms/services/LmsService.java
+++ LMS/src/main/java/com/lms/services/LmsService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 import com.lms.dao.LmsRepository;
 import com.lms.models.Book;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Service
 public class LmsService {
@@ -14,6 +16,14 @@ public class LmsService {
 	@Autowired									//ensure dependencies from LmsRepository are loaded
 	private LmsRepository lmsRepository;		//create instance of LmsRepository(dao)
 	
-	private Collection<Book> 
-
+	public Collection<Book> findAllBooks(){
+		List<Book> books = new ArrayList<>();
+		for (Book book : lmsRepository.findAll()) {
+			books.add(book);
+		}
+		return books;
+	}
+	
+	
+	
 }
